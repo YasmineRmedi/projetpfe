733ebed8a1cf84a530ae240304f857b875191d3e
YasmineRmedi <yasminermedi024@gmail.com>
2023-02-21 14:31:10 +0100

    facture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FactureModule } from './facture/facture.module';
 
 @Module({
-  imports: [],
+  imports: [FactureModule],
   controllers: [AppController],
   providers: [AppService],
 })
