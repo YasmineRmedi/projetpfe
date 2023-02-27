38d66a508f7c12bb729b6740610df85e69d8f431
YasmineRmedi <yasminermedi024@gmail.com>
2023-02-27 15:27:25 +0100

    configuration 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { Facture } from './facture/entities/facture.entity';
 import { FactureModule } from './facture/facture.module';
 import { SubscriberModule } from './subscriber/subscriber.module';
@@ -11,16 +12,10 @@ import { DeclarationModule } from './declaration/declaration.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(/*{
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'admin',
-      database: 'pfe',
-      autoLoadEntities: true,
-      synchronize: true,
-    }*/),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.values.env',
+    }),
     FactureModule,
     SubscriberModule,
     CompanyModule,
--- src/database.module.ts
+++ src/database.module.ts
@@ -5,7 +5,7 @@ import { ConnectionOptions } from 'typeorm/connection/ConnectionOptions';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 @Module({
   imports: [
-    ConfigModule, // Add this line
+    ConfigModule,
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
@@ -16,7 +16,6 @@ import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConne
           username: configService.get<string>('DB_USER'),
           password: configService.get<string>('DB_PASSWORD'),
           database: configService.get<string>('DB_NAME'),
-          // Other database options...
         };
         return connectionOptions;
       },
